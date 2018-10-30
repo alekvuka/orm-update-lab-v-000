e65c16dc2a6735238073f3cb7a3a99491bd32d27
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-30 16:52:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
 
     def self.new_from_db(row)
       #binding.pry
-      student = Student.new(row[1], row[0], row[2])
+      student = Student.new(row[2], row[0], row[1])
       student
     end
 
