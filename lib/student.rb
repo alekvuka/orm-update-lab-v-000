3f9738e2c0ba3aa4f966f7d49c4501d621c5feba
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-30 16:46:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,11 +46,10 @@ class Student
       end
     end
 
-    def self.create(:name, :grade)
+    def self.create(name:, grade:)
       student = Student.new(name, grade)
-      student.save 
+      student.save
       student
-
     end
 
 
