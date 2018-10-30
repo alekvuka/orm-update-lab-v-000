6ad2fd5874e2015def2f6d373c7dc5e82452837c
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-30 16:36:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,9 +24,14 @@ class Student
     end
 
     def self.drop_table
-      
+      sql = <<-SQL
+        DROP TABLE students
+        SQL
+
+      DB[:conn].execute(sql)
 
 
-    end 
+
+    end
 
 end
